7a1295bc971a3e64bf91a6656ee939d1bad94ecf
Tomas <discipleartem@gmail.com>
2024-09-21 17:56:28 +0300

    refact 1

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from random import randint
 
+VERTICAL_COORDINATES = ('a', 'b', 'c')
 
-VERTICAL_COORDINATS = ('a', 'b', 'c')
+
+def select_char():
+    return input('Select char (x, 0): '.strip(' ').lower())
+
 
 def get_user_char():
-    user_char = input('Select char (x, 0): ').strip(' ').lower()
-    while user_char not in ('x', '0'):
+    char = select_char()
+
+    while char not in ('x', '0'):
         print('Not available char')
-        user_char = input('Select char (x, 0): ').strip(' ').lower()
-    return user_char
+        char = select_char()
+    return char
+
 
 def show_field(field):
     print(' ', '1', '2', '3')
-    for y, v in enumerate(VERTICAL_COORDINATS):
+    for y, v in enumerate(VERTICAL_COORDINATES):
         print(v, ' '.join(field[y]))
 
 
@@ -23,22 +29,27 @@ def is_draw(field):
     return count == 0
 
 
+def input_coordinates():
+    return input('Input coordinates: ').lower().strip(' ')
+
+
 def get_user_position(field):
     real_x, real_y = None, None
-    while True:
-        coordinats = input('Input coordinats: ').lower().strip(' ')
-        y, x = tuple(coordinats)
 
-        if int(x) not in range(1, 4) or y not in VERTICAL_COORDINATS:
-            print('Not available coordinats')
+    while True:
+        y, x = tuple(input_coordinates())
+
+        if int(x) not in range(1, 4) or y not in VERTICAL_COORDINATES:
+            print('Not available coordinates')
             continue
 
-        real_x, real_y = int(x) - 1, VERTICAL_COORDINATS.index(y)
+        real_x, real_y = int(x) - 1, VERTICAL_COORDINATES.index(y)
         if field[real_y][real_x] == '_':
             break
         else:
             print('Position not empty')
     return real_x, real_y
+
 
 def get_opponent_char(char):
     return '0' if char == 'x' else 'x'
@@ -58,19 +69,19 @@ def is_win(char, field):
             return True
 
     #check diagonal
-    diag1 = [field[0][0], field[1][1], field[2][2]]
-    if opponent_char not in diag1 and '_' not in diag1:
+    first_cross_line = [field[0][0], field[1][1], field[2][2]]
+    if opponent_char not in first_cross_line and '_' not in first_cross_line:
         return True
 
-    diag2 = [field[0][2], field[1][1], field[2][0]]
-    if opponent_char not in diag2 and '_' not in diag2:
+    second_cross_line = [field[0][2], field[1][1], field[2][0]]
+    if opponent_char not in second_cross_line and '_' not in second_cross_line:
         return True
 
     return False
 
-field = [
+static_field = [
     ['_' for x in range(3)] for y in range(3)
-          ]
+]
 
 user_char = get_user_char()
 computer_char = get_opponent_char(user_char)
@@ -84,23 +95,25 @@ def get_computer_position(field):
 
 
 while True:
-    show_field(field)
-    if is_draw(field):
+    show_field(static_field)
+    if is_draw(static_field):
         print('is Draw')
         break
 
 
-    x, y = get_user_position(field)
-    field[y][x] = user_char
+    x, y = get_user_position(static_field)
+    static_field[y][x] = user_char
 
-    if is_win(user_char, field):
+    if is_win(user_char, static_field):
+        show_field(static_field)
         print('You win')
         break
 
 
-    x, y = get_computer_position(field)
-    field[y][x] = computer_char
+    x, y = get_computer_position(static_field)
+    static_field[y][x] = computer_char
 
-    if is_win(computer_char, field):
+    if is_win(computer_char, static_field):
+        show_field(static_field)
         print('You lose')
         break
