c3eb868acd287d60f3751b217ae32804b810d1b8
Tomas <discipleartem@gmail.com>
2024-09-22 23:20:26 +0300

    Refactor and simplify user input validation  def get_user_char
    Reorganized functions to validate user input using a helper function `is_valid_char`. Updated variable names for better readability and consistency, changing instances of "char" to "choice". Improved prompt message clarity for invalid characters.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from random import randint
 
 VERTICAL_COORDINATES = ('a', 'b', 'c')
+ALLOWED_CHARS = ('x', '0')
 
+
+def is_valid_char(char):
+    return char in ALLOWED_CHARS
 
 def select_char():
     return input('Select char (x, 0): '.strip(' ').lower())
 
 
-def get_user_char():
-    char = select_char()
-
-    while char not in ('x', '0'):
-        print('Not available char')
-        char = select_char()
-    return char
+def get_user_input():
+    user_char = select_char()
+    while not is_valid_char(user_char):
+        print('Character not available')
+        user_char = select_char()
+    return user_char
 
 
 def show_field(field):
@@ -83,8 +86,8 @@ static_field = [
     ['_' for x in range(3)] for y in range(3)
 ]
 
-user_char = get_user_char()
-computer_char = get_opponent_char(user_char)
+user_choice = get_user_input()
+computer_choice = get_opponent_char(user_choice)
 
 
 def get_computer_position(field):
@@ -102,18 +105,18 @@ while True:
 
 
     x, y = get_user_position(static_field)
-    static_field[y][x] = user_char
+    static_field[y][x] = user_choice
 
-    if is_win(user_char, static_field):
+    if is_win(user_choice, static_field):
         show_field(static_field)
         print('You win')
         break
 
 
     x, y = get_computer_position(static_field)
-    static_field[y][x] = computer_char
+    static_field[y][x] = computer_choice
 
-    if is_win(computer_char, static_field):
+    if is_win(computer_choice, static_field):
         show_field(static_field)
         print('You lose')
         break
