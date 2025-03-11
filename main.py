95bffc3cd1933cd42378860bec88f18b6e9d876a
Tomas <discipleartem@gmail.com>
2025-03-11 12:01:00 +0200

    - Создана отдельная функция set_field() для инициализации игрового поля
    - Переименованы переменные в константы (LETTERS, NUMBERS)
    
    - Улучшена читаемость кода в display_field()
    
    - Добавлены поясняющие комментарии
    
    - Удалена глобальная переменная FIELD

--- main.py
+++ main.py
@@ -30,24 +30,28 @@
 0 4 8   2 4 6
 """
 
-letters = ['a', 'b', 'c']
-numbers = ['1', '2', '3']
-keys = [letter + number for letter in letters for number in numbers]
-default_value = None
-FIELD = dict.fromkeys(keys, default_value)
+LETTERS = ['a', 'b', 'c']
+NUMBERS = ['1', '2', '3']
 ALLOWED_SYMBOLS = ('x', 'o')
+
 player_1 = {'name': 'Игрок 1', 'symbol': None}
 player_2 = {'name': 'Игрок 2', 'symbol': None}
+
 
 
 def game():
     print('Игра крестики-нолики')
     display_field()
 
-    global player_1, player_2
+    global player_1, player_2 # чтобы изменить значения переменных внутри функции
     player_1, player_2 = choose_symbol(player_1, player_2)
 
+def set_field():
+    keys = [letter + number for letter in LETTERS for number in NUMBERS]
+    default_value = None
 
+    field = dict.fromkeys(keys, default_value)
+    return field
 
 def choose_symbol(first_player: dict, second_player: dict) -> tuple[dict, dict]:
     first_player['symbol'] = get_player_symbol(first_player['name'])
@@ -67,20 +71,21 @@ def get_player_symbol(player_name: str) -> str:
         print('Неверный символ. Пожалуйста, выберите x или o.')
 
 def display_field():
-    field = {key: "_" if value is None else value for key, value in FIELD.items()}
+    origin_field = set_field()
+    modified_field = {key: "_" if value is None else value for key, value in origin_field.items()}
 
     print('  ', end=' ') # отступ в начале строки для a, b, c
 
-    for letter in letters:
+    for letter in LETTERS:
         print(letter, end=' | ') # разделитель между буквами
 
     print('', end='\n') # перенос строки
 
-    for number in numbers:
+    for number in NUMBERS:
         print(number, end='  ')# отступ между цифрами
 
-        for letter in letters:
-            print(field[letter + number], end=" | ") # разделитель между клетками
+        for letter in LETTERS:
+            print(modified_field[letter + number], end=" | ") # разделитель между клетками
 
         print('', end='\n') # перенос после каждой строки
 
